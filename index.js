19f275640d62db9349955aa42c2dc3280822f01c
gustavo-0101 <guga-rd@hotmail.com>
2020-07-07 18:18:17 -0300

    cleaning the scraps code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let scrapsField = document.getElementById("scrapsField");
-let btnInput = document.getElementById("inputButton");
+let addScrapBtn = document.getElementById("inputButton");
 let titleInput = document.getElementById("titleInput");
 let messageField = document.getElementById("messageField");
 
@@ -13,45 +13,16 @@ function renderScraps() {
   }
 
   for (const scrap of scraps) {
-    let cardTitle = document.createTextNode(scrap.title);
-    let cardText = document.createTextNode(scrap.message);
-
-    let card = document.createElement("div");
-    card.className = "message-cards card text-white bg-dark m-2 col-3";
-
-    let cardBody = document.createElement("div");
-    cardBody.className = "card-body p-2";
-
-    let cardHeader = document.createElement("div");
-    cardHeader.appendChild(cardTitle);
-    cardHeader.className = "card-header font-weight-bold text-center";
-
-    let cardParagraph = document.createElement("p");
-    cardParagraph.appendChild(cardText);
-    cardParagraph.className = "card-text";
-
-    cardBody.appendChild(cardParagraph);
-    card.appendChild(cardHeader);
-    card.appendChild(cardBody);
-    scrapsField.appendChild(card);
+    let position = scraps.indexOf(scrap);
+    scrapsField.innerHTML += createScrapCard(
+      scrap.title,
+      scrap.message,
+      position
+    );
   }
 }
 
-messageField.onkeypress = function (event) {
-  if (event.keyCode === 10) {
-    createScrap();
-  }
-};
-
-titleInput.onkeypress = function (event) {
-  if (event.keyCode === 13) {
-    createScrap();
-  }
-};
-
-renderScraps();
-
-function createScrap() {
+function addScrap() {
   if (messageField.value === "" || titleInput.value.length > 30) {
     alert("DADOS INVÁLIDOS");
   } else {
@@ -66,4 +37,32 @@ function createScrap() {
   }
 }
 
-btnInput.onclick = createScrap;
+messageField.onkeypress = function (event) {
+  if (event.keyCode === 10) {
+    addScrap();
+  }
+};
+
+titleInput.onkeypress = function (event) {
+  if (event.keyCode === 13) {
+    addScrap();
+  }
+};
+
+
+
+function createScrapCard(title, message, position) {
+  return `
+  <div class="message-cards card text-white bg-dark m-2 col-3">
+    <div class="card-header font-weight-bold text-center">${title}</div>
+    <div class="card-body">
+      <p class="card-text">
+        ${message}
+      </p>
+    </div>
+  </div>
+  `;
+}
+
+renderScraps();
+addScrapBtn.onclick = addScrap;
