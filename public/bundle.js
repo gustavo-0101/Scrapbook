a244921ca1c59dddf1d8de91e88efbaf998732c7
gustavo-0101 <guga-rd@hotmail.com>
2020-08-11 20:59:32 -0300

    webpack config

--- public/bundle.js
+++ public/bundle.js
@@ -1,164 +1,113 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
 "use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _soma__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./soma */ \"./src/soma.js\");\n// class TaskList {\n//   constructor() {\n//     this.titleInput = document.getElementById(\"titleInput\");\n//     this.messageInput = document.getElementById(\"messageField\");\n//     this.addBtn = document.getElementById(\"inputButton\");\n//     this.scrapsField = document.getElementById(\"scrapsField\");\n//     this.editTitleInput = document.getElementById(\"editTitleInput\");\n//     this.editMessageField = document.getElementById(\"editMessageField\");\n//     this.btnSaveEdit = document.getElementById(\"saveEdit\");\n//     this.scraps = [];\n//     this.registerAddScrapBtnEvent();\n//   }\n//   generateScrapId() {\n//     return this.scraps.length + 1;\n//   }\n//   registerAddScrapBtnEvent() {\n//     this.addBtn.onclick = () => this.addNewScrap();\n//   }\n//   setButtonsEvents() {\n//     document.querySelectorAll(\".delete-button\").forEach((item) => {\n//       item.onclick = (event) => this.deleteScrap(event);\n//     });\n//     document.querySelectorAll(\".edit-button\").forEach((item) => {\n//       item.onclick = (event) => this.openEditModal(event);\n//     });\n//   }\n//   renderScraps() {\n//     this.scrapsField.innerHTML = \"\";\n//     for (const scrap of this.scraps) {\n//       const cardHtml = this.createScrapCard(\n//         scrap.id,\n//         scrap.title,\n//         scrap.message\n//       );\n//       this.insertHtml(cardHtml);\n//     }\n//     this.setButtonsEvents();\n//   }\n//   addNewScrap() {\n//     if (this.titleInput.value === \"\" || this.titleInput.value.length > 100) {\n//       alert(\"Insira um título (de até 100 caracteres)\");\n//     } else if (this.messageInput.value === \"\") {\n//       alert(\"Você não pode deixar a aba 'Mensagem' em branco!\");\n//     } else {\n//       const id = this.generateScrapId();\n//       const title = this.titleInput.value;\n//       const message = this.messageInput.value;\n//       this.titleInput.value = \"\";\n//       this.messageInput.value = \"\";\n//       this.scraps.push({ id, title, message });\n//       this.renderScraps();\n//     }\n//   }\n//   deleteScrap(event) {\n//     event.path[2].remove();\n//     const scrapId = event.path[2].getAttribute(\"id-scrap\");\n//     const scrapIndex = this.scraps.findIndex((item) => {\n//       return item.id == scrapId;\n//     });\n//     this.scraps.splice(scrapIndex, 1);\n//   }\n//   openEditModal(event) {\n//     $(\"#editModal\").modal(\"toggle\");\n//     const scrapId = event.path[2].getAttribute(\"id-scrap\");\n//     const scrapIndex = this.scraps.findIndex((item) => {\n//       return item.id == scrapId;\n//     });\n//     this.editTitleInput.value = this.scraps[scrapIndex].title;\n//     this.editMessageField.value = this.scraps[scrapIndex].message;\n//     this.btnSaveEdit.onclick = () => this.saveChanges(scrapIndex);\n//   }\n//   saveChanges(scrapIndex) {\n//     if (this.editMessageField.value === \"\") {\n//       alert(\"Você não pode deixar a aba 'Mensagem' em branco!\");\n//     } else if (\n//       this.editTitleInput.value === \"\" ||\n//       this.editTitleInput.value.length > 100\n//     ) {\n//       alert(\"Insira um título (com até 100 caracteres)\");\n//     } else {\n//       let title = this.editTitleInput.value;\n//       let message = this.editMessageField.value;\n//       this.scraps[scrapIndex] = { title, message };\n//       this.renderScraps();\n//       $(\"#editModal\").modal(\"hide\");\n//     }\n//   }\n//   insertHtml(html) {\n//     1;\n//     this.scrapsField.innerHTML += html;\n//   }\n//   createScrapCard(id, title, message) {\n//     return `\n//       <div class=\"message-cards card text-white bg-dark m-2 col-3\" id-scrap=\"${id}\">\n//         <div class=\"card-header font-weight-bold text-center\">${title}</div>\n//           <p class=\"card-text\">\n//             ${message}\n//           </p>\n//           <div class=\"w-100 d-flex justify-content-center pr-2 pb-2\">\n//             <button type=\"button\" class=\"btn btn-primary btn-sm bg-danger delete-button\">Excluir</button>\n//             <button type=\"button\" class=\"btn btn-secondary btn-sm bg-warning text-dark edit-button\">Editar</button>\n//         </div>\n//       </div>\n//       `;\n//   }\n// }\n// new TaskList();\n\nalert(Object(_soma__WEBPACK_IMPORTED_MODULE_0__[\"soma\"])(1, 2));\n\n//# sourceURL=webpack:///./src/index.js?");
 
-function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+/***/ }),
 
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+/***/ "./src/soma.js":
+/*!*********************!*\
+  !*** ./src/soma.js ***!
+  \*********************/
+/*! exports provided: soma */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"soma\", function() { return soma; });\nvar soma = function soma(a, b) {\n  return a + b;\n};\n\n//# sourceURL=webpack:///./src/soma.js?");
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/***/ })
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var TaskList = /*#__PURE__*/function () {
-  function TaskList() {
-    _classCallCheck(this, TaskList);
-
-    this.titleInput = document.getElementById("titleInput");
-    this.messageInput = document.getElementById("messageField");
-    this.addBtn = document.getElementById("inputButton");
-    this.scrapsField = document.getElementById("scrapsField");
-    this.editTitleInput = document.getElementById("editTitleInput");
-    this.editMessageField = document.getElementById("editMessageField");
-    this.btnSaveEdit = document.getElementById("saveEdit");
-    this.scraps = [];
-    this.registerAddScrapBtnEvent();
-  }
-
-  _createClass(TaskList, [{
-    key: "generateScrapId",
-    value: function generateScrapId() {
-      return this.scraps.length + 1;
-    }
-  }, {
-    key: "registerAddScrapBtnEvent",
-    value: function registerAddScrapBtnEvent() {
-      var _this = this;
-
-      this.addBtn.onclick = function () {
-        return _this.addNewScrap();
-      };
-    }
-  }, {
-    key: "setButtonsEvents",
-    value: function setButtonsEvents() {
-      var _this2 = this;
-
-      document.querySelectorAll(".delete-button").forEach(function (item) {
-        item.onclick = function (event) {
-          return _this2.deleteScrap(event);
-        };
-      });
-      document.querySelectorAll(".edit-button").forEach(function (item) {
-        item.onclick = function (event) {
-          return _this2.openEditModal(event);
-        };
-      });
-    }
-  }, {
-    key: "renderScraps",
-    value: function renderScraps() {
-      this.scrapsField.innerHTML = "";
-
-      var _iterator = _createForOfIteratorHelper(this.scraps),
-          _step;
-
-      try {
-        for (_iterator.s(); !(_step = _iterator.n()).done;) {
-          var scrap = _step.value;
-          var cardHtml = this.createScrapCard(scrap.id, scrap.title, scrap.message);
-          this.insertHtml(cardHtml);
-        }
-      } catch (err) {
-        _iterator.e(err);
-      } finally {
-        _iterator.f();
-      }
-
-      this.setButtonsEvents();
-    }
-  }, {
-    key: "addNewScrap",
-    value: function addNewScrap() {
-      if (this.titleInput.value === "" || this.titleInput.value.length > 100) {
-        alert("Insira um título (de até 100 caracteres)");
-      } else if (this.messageInput.value === "") {
-        alert("Você não pode deixar a aba 'Mensagem' em branco!");
-      } else {
-        var id = this.generateScrapId();
-        var title = this.titleInput.value;
-        var message = this.messageInput.value;
-        this.titleInput.value = "";
-        this.messageInput.value = "";
-        this.scraps.push({
-          id: id,
-          title: title,
-          message: message
-        });
-        this.renderScraps();
-      }
-    }
-  }, {
-    key: "deleteScrap",
-    value: function deleteScrap(event) {
-      event.path[2].remove();
-      var scrapId = event.path[2].getAttribute("id-scrap");
-      var scrapIndex = this.scraps.findIndex(function (item) {
-        return item.id == scrapId;
-      });
-      this.scraps.splice(scrapIndex, 1);
-    }
-  }, {
-    key: "openEditModal",
-    value: function openEditModal(event) {
-      var _this3 = this;
-
-      $("#editModal").modal("toggle");
-      var scrapId = event.path[2].getAttribute("id-scrap");
-      var scrapIndex = this.scraps.findIndex(function (item) {
-        return item.id == scrapId;
-      });
-      this.editTitleInput.value = this.scraps[scrapIndex].title;
-      this.editMessageField.value = this.scraps[scrapIndex].message;
-
-      this.btnSaveEdit.onclick = function () {
-        return _this3.saveChanges(scrapIndex);
-      };
-    }
-  }, {
-    key: "saveChanges",
-    value: function saveChanges(scrapIndex) {
-      if (this.editMessageField.value === "") {
-        alert("Você não pode deixar a aba 'Mensagem' em branco!");
-      } else if (this.editTitleInput.value === "" || this.editTitleInput.value.length > 100) {
-        alert("Insira um título (com até 100 caracteres)");
-      } else {
-        var title = this.editTitleInput.value;
-        var message = this.editMessageField.value;
-        this.scraps[scrapIndex] = {
-          title: title,
-          message: message
-        };
-        this.renderScraps();
-        $("#editModal").modal("hide");
-      }
-    }
-  }, {
-    key: "insertHtml",
-    value: function insertHtml(html) {
-      1;
-      this.scrapsField.innerHTML += html;
-    }
-  }, {
-    key: "createScrapCard",
-    value: function createScrapCard(id, title, message) {
-      return "\n      <div class=\"message-cards card text-white bg-dark m-2 col-3\" id-scrap=\"".concat(id, "\">\n        <div class=\"card-header font-weight-bold text-center\">").concat(title, "</div>\n          <p class=\"card-text\">\n            ").concat(message, "\n          </p>\n          <div class=\"w-100 d-flex justify-content-center pr-2 pb-2\">\n            <button type=\"button\" class=\"btn btn-primary btn-sm bg-danger delete-button\">Excluir</button>\n            <button type=\"button\" class=\"btn btn-secondary btn-sm bg-warning text-dark edit-button\">Editar</button>\n        </div>\n      </div>\n      ");
-    }
-  }]);
-
-  return TaskList;
-}();
-
-new TaskList();
+/******/ });
--- src/index.js
+++ src/index.js
@@ -1,131 +1,135 @@
-class TaskList {
-  constructor() {
-    this.titleInput = document.getElementById("titleInput");
-    this.messageInput = document.getElementById("messageField");
-    this.addBtn = document.getElementById("inputButton");
-    this.scrapsField = document.getElementById("scrapsField");
-    this.editTitleInput = document.getElementById("editTitleInput");
-    this.editMessageField = document.getElementById("editMessageField");
-    this.btnSaveEdit = document.getElementById("saveEdit");
+// class TaskList {
+//   constructor() {
+//     this.titleInput = document.getElementById("titleInput");
+//     this.messageInput = document.getElementById("messageField");
+//     this.addBtn = document.getElementById("inputButton");
+//     this.scrapsField = document.getElementById("scrapsField");
+//     this.editTitleInput = document.getElementById("editTitleInput");
+//     this.editMessageField = document.getElementById("editMessageField");
+//     this.btnSaveEdit = document.getElementById("saveEdit");
 
-    this.scraps = [];
+//     this.scraps = [];
 
-    this.registerAddScrapBtnEvent();
-  }
+//     this.registerAddScrapBtnEvent();
+//   }
 
-  generateScrapId() {
-    return this.scraps.length + 1;
-  }
+//   generateScrapId() {
+//     return this.scraps.length + 1;
+//   }
 
-  registerAddScrapBtnEvent() {
-    this.addBtn.onclick = () => this.addNewScrap();
-  }
+//   registerAddScrapBtnEvent() {
+//     this.addBtn.onclick = () => this.addNewScrap();
+//   }
 
-  setButtonsEvents() {
-    document.querySelectorAll(".delete-button").forEach((item) => {
-      item.onclick = (event) => this.deleteScrap(event);
-    });
-    document.querySelectorAll(".edit-button").forEach((item) => {
-      item.onclick = (event) => this.openEditModal(event);
-    });
-  }
+//   setButtonsEvents() {
+//     document.querySelectorAll(".delete-button").forEach((item) => {
+//       item.onclick = (event) => this.deleteScrap(event);
+//     });
+//     document.querySelectorAll(".edit-button").forEach((item) => {
+//       item.onclick = (event) => this.openEditModal(event);
+//     });
+//   }
 
-  renderScraps() {
-    this.scrapsField.innerHTML = "";
+//   renderScraps() {
+//     this.scrapsField.innerHTML = "";
 
-    for (const scrap of this.scraps) {
-      const cardHtml = this.createScrapCard(
-        scrap.id,
-        scrap.title,
-        scrap.message
-      );
-      this.insertHtml(cardHtml);
-    }
-    this.setButtonsEvents();
-  }
+//     for (const scrap of this.scraps) {
+//       const cardHtml = this.createScrapCard(
+//         scrap.id,
+//         scrap.title,
+//         scrap.message
+//       );
+//       this.insertHtml(cardHtml);
+//     }
+//     this.setButtonsEvents();
+//   }
 
-  addNewScrap() {
-    if (this.titleInput.value === "" || this.titleInput.value.length > 100) {
-      alert("Insira um título (de até 100 caracteres)");
-    } else if (this.messageInput.value === "") {
-      alert("Você não pode deixar a aba 'Mensagem' em branco!");
-    } else {
-      const id = this.generateScrapId();
-      const title = this.titleInput.value;
-      const message = this.messageInput.value;
+//   addNewScrap() {
+//     if (this.titleInput.value === "" || this.titleInput.value.length > 100) {
+//       alert("Insira um título (de até 100 caracteres)");
+//     } else if (this.messageInput.value === "") {
+//       alert("Você não pode deixar a aba 'Mensagem' em branco!");
+//     } else {
+//       const id = this.generateScrapId();
+//       const title = this.titleInput.value;
+//       const message = this.messageInput.value;
 
-      this.titleInput.value = "";
-      this.messageInput.value = "";
+//       this.titleInput.value = "";
+//       this.messageInput.value = "";
 
-      this.scraps.push({ id, title, message });
+//       this.scraps.push({ id, title, message });
 
-      this.renderScraps();
-    }
-  }
+//       this.renderScraps();
+//     }
+//   }
 
-  deleteScrap(event) {
-    event.path[2].remove();
+//   deleteScrap(event) {
+//     event.path[2].remove();
 
-    const scrapId = event.path[2].getAttribute("id-scrap");
+//     const scrapId = event.path[2].getAttribute("id-scrap");
 
-    const scrapIndex = this.scraps.findIndex((item) => {
-      return item.id == scrapId;
-    });
+//     const scrapIndex = this.scraps.findIndex((item) => {
+//       return item.id == scrapId;
+//     });
 
-    this.scraps.splice(scrapIndex, 1);
-  }
+//     this.scraps.splice(scrapIndex, 1);
+//   }
 
-  openEditModal(event) {
-    $("#editModal").modal("toggle");
+//   openEditModal(event) {
+//     $("#editModal").modal("toggle");
 
-    const scrapId = event.path[2].getAttribute("id-scrap");
+//     const scrapId = event.path[2].getAttribute("id-scrap");
 
-    const scrapIndex = this.scraps.findIndex((item) => {
-      return item.id == scrapId;
-    });
+//     const scrapIndex = this.scraps.findIndex((item) => {
+//       return item.id == scrapId;
+//     });
 
-    this.editTitleInput.value = this.scraps[scrapIndex].title;
-    this.editMessageField.value = this.scraps[scrapIndex].message;
+//     this.editTitleInput.value = this.scraps[scrapIndex].title;
+//     this.editMessageField.value = this.scraps[scrapIndex].message;
 
-    this.btnSaveEdit.onclick = () => this.saveChanges(scrapIndex);
-  }
+//     this.btnSaveEdit.onclick = () => this.saveChanges(scrapIndex);
+//   }
 
-  saveChanges(scrapIndex) {
-    if (this.editMessageField.value === "") {
-      alert("Você não pode deixar a aba 'Mensagem' em branco!");
-    } else if (
-      this.editTitleInput.value === "" ||
-      this.editTitleInput.value.length > 100
-    ) {
-      alert("Insira um título (com até 100 caracteres)");
-    } else {
-      let title = this.editTitleInput.value;
-      let message = this.editMessageField.value;
+//   saveChanges(scrapIndex) {
+//     if (this.editMessageField.value === "") {
+//       alert("Você não pode deixar a aba 'Mensagem' em branco!");
+//     } else if (
+//       this.editTitleInput.value === "" ||
+//       this.editTitleInput.value.length > 100
+//     ) {
+//       alert("Insira um título (com até 100 caracteres)");
+//     } else {
+//       let title = this.editTitleInput.value;
+//       let message = this.editMessageField.value;
 
-      this.scraps[scrapIndex] = { title, message };
-      this.renderScraps();
-      $("#editModal").modal("hide");
-    }
-  }
+//       this.scraps[scrapIndex] = { title, message };
+//       this.renderScraps();
+//       $("#editModal").modal("hide");
+//     }
+//   }
 
-  insertHtml(html) {
-    1;
-    this.scrapsField.innerHTML += html;
-  }
+//   insertHtml(html) {
+//     1;
+//     this.scrapsField.innerHTML += html;
+//   }
 
-  createScrapCard(id, title, message) {
-    return `
-      <div class="message-cards card text-white bg-dark m-2 col-3" id-scrap="${id}">
-        <div class="card-header font-weight-bold text-center">${title}</div>
-          <p class="card-text">
-            ${message}
-          </p>
-          <div class="w-100 d-flex justify-content-center pr-2 pb-2">
-            <button type="button" class="btn btn-primary btn-sm bg-danger delete-button">Excluir</button>
-            <button type="button" class="btn btn-secondary btn-sm bg-warning text-dark edit-button">Editar</button>
-        </div>
-      </div>
-      `;
-  }
-}
-new TaskList();
+//   createScrapCard(id, title, message) {
+//     return `
+//       <div class="message-cards card text-white bg-dark m-2 col-3" id-scrap="${id}">
+//         <div class="card-header font-weight-bold text-center">${title}</div>
+//           <p class="card-text">
+//             ${message}
+//           </p>
+//           <div class="w-100 d-flex justify-content-center pr-2 pb-2">
+//             <button type="button" class="btn btn-primary btn-sm bg-danger delete-button">Excluir</button>
+//             <button type="button" class="btn btn-secondary btn-sm bg-warning text-dark edit-button">Editar</button>
+//         </div>
+//       </div>
+//       `;
+//   }
+// }
+// new TaskList();
+
+import { soma } from "./soma";
+
+alert(soma(1, 2));
